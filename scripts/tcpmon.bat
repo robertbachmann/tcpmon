51e3a8205a0866dee5478b98bef7a3bd34973fbe
Robert Bachmann <rb@robertbachmann.at>
2014-06-20 20:42:37 +0200

    Add Main-Class to manifest

--- scripts/tcpmon.bat
+++
@@ -1,2 +0,0 @@
-rem Run the java exe with tcpmon jar
-java -cp ./tcpmon-@tcpmon_version@.jar org.apache.ws.commons.tcpmon.TCPMon %*
--- scripts/tcpmon.sh
+++
@@ -1,1 +0,0 @@
-java -cp ./tcpmon-@tcpmon_version@.jar org.apache.ws.commons.tcpmon.TCPMon $*
