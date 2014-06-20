e7a23601c11d424e592d7da456c6f88598eecd73
Robert Bachmann <rb@robertbachmann.at>
2014-06-20 20:04:41 +0200

    Initial import from tcpmon-1.0-src.zip
    Excluding xdocs/ for now

---
+++ scripts/tcpmon.bat
@@ -0,0 +1,2 @@
+rem Run the java exe with tcpmon jar
+java -cp ./tcpmon-@tcpmon_version@.jar org.apache.ws.commons.tcpmon.TCPMon %*
---
+++ scripts/tcpmon.sh
@@ -0,0 +1,1 @@
+java -cp ./tcpmon-@tcpmon_version@.jar org.apache.ws.commons.tcpmon.TCPMon $*
